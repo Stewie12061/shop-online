bb0f80eded149d96ab46d65268d67c949e988ba3
Stewie12061 <nhut170501@gmail.com>
2021-05-24 19:26:54 +0700

    build home

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get '/contact', controller: :contact, action: :index
+  get '/about', controller: :about, action: :index
+  get '/products', controller: :products, action: :index
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
