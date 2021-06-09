11617e3c9dd2fb1df572048f8e6bd601a5d7ce75
Stewie12061 <nhut170501@gmail.com>
2021-06-09 19:21:55 +0700

    add language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale
+    before_action :fetch_language
     private
     def set_locale
         p session[:locale]
-        locale=session[:locale] || :vi
+        locale=session[:locale] || :en
         p locale
         I18n.locale=locale.to_sym
+    end
+
+    def fetch_language
+        @languages = [{value: 'Tiếng Việt', id: :vi}, {value: 'English', id: :en}].map{|l| OpenStruct.new(l)}
     end
 end
